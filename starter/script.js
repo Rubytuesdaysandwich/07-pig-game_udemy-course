ad48cba017f1b912d865b2bad7c0ad777709b34c
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-08-03 14:12:20 -0600

    update

--- starter/script.js
+++ starter/script.js
@@ -1,10 +1,48 @@
 'use strict';
 
 //selecting elements
+const player0El = document.querySelector('player--0');
+const player1El = document.querySelector('player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
+//starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+//rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  //1. generating random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+
+  //2.display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  //3.check for rolled 1: if true,switch to next player
+  if (dice !== 1) {
+    //if not a one add dice to current score
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    current0El.textContent = currentScore;
+  } else {
+    //switch to next player
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0; // if equal to 0 make 1 or if === 1 then make 0
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+  }
+});
